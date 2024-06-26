67a8269d5bc684d41fc446654afe4641ba0d6561
Erfan Ansari <dev.erfanansari@gmail.com>
2024-06-27 00:51:31 +0330

    chore: add work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,10 +108,13 @@ export default function Page() {
                 <CardHeader>
                   <div className="flex items-center justify-between gap-x-2 text-base">
                     <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
-                      <a className="hover:underline" href={work.link}>
-                        {work.company}
-                      </a>
-
+                      {work.href ? (
+                        <a className="hover:underline" href={work.href}>
+                          {work.company}
+                        </a>
+                      ) : (
+                        <span>{work.company}</span>
+                      )}
                       <span className="inline-flex gap-x-1">
                         {work.badges.map((badge) => (
                           <Badge
