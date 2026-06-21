a750b122c55db058aa9dc73ac5a0d57e066e314e
Erfan Ansari <dev.erfanansari@gmail.com>
2026-06-21 23:56:08 +0330

    feat: add fade in animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,214 +19,234 @@ export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 md:p-16 print:p-12">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-4">
-        <div className="flex items-center justify-between">
-          <div className="flex-1 space-y-1.5">
-            <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
-            <p className="max-w-md text-pretty font-mono text-sm text-foreground/80 print:text-[12px]">
-              {RESUME_DATA.about}
-            </p>
-            {RESUME_DATA.location && RESUME_DATA.locationLink && (
-              <p className="max-w-md items-center text-pretty font-mono text-xs text-foreground/80">
-                <a
-                  className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
-                  href={RESUME_DATA.locationLink}
-                  target="_blank"
-                >
-                  <GlobeIcon className="size-3" />
-                  {RESUME_DATA.location}
-                </a>
+        <div className="animate-fade-in" style={{ animationDelay: "0ms" }}>
+          <div className="flex items-center justify-between">
+            <div className="flex-1 space-y-1.5">
+              <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
+              <p className="max-w-md text-pretty font-mono text-sm text-foreground/80 print:text-[12px]">
+                {RESUME_DATA.about}
               </p>
-            )}
-            <div className="flex gap-x-1 pt-1 font-mono text-sm text-foreground/80 print:hidden">
-              {RESUME_DATA.contact.email ? (
-                <Button
-                  className="size-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
+              {RESUME_DATA.location && RESUME_DATA.locationLink && (
+                <p className="max-w-md items-center text-pretty font-mono text-xs text-foreground/80">
+                  <a
+                    className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
+                    href={RESUME_DATA.locationLink}
+                    target="_blank"
+                  >
+                    <GlobeIcon className="size-3" />
+                    {RESUME_DATA.location}
+                  </a>
+                </p>
+              )}
+              <div className="flex gap-x-1 pt-1 font-mono text-sm text-foreground/80 print:hidden">
+                {RESUME_DATA.contact.email ? (
+                  <Button
+                    className="size-8"
+                    variant="outline"
+                    size="icon"
+                    asChild
+                  >
+                    <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                      <MailIcon className="size-4" />
+                    </a>
+                  </Button>
+                ) : null}
+                {RESUME_DATA.contact.tel ? (
+                  <Button
+                    className="size-8"
+                    variant="outline"
+                    size="icon"
+                    asChild
+                  >
+                    <a href={`tel:${RESUME_DATA.contact.tel}`}>
+                      <PhoneIcon className="size-4" />
+                    </a>
+                  </Button>
+                ) : null}
+                {RESUME_DATA.contact.social.map((social) => (
+                  <Button
+                    key={social.title}
+                    className="size-8"
+                    variant="outline"
+                    size="icon"
+                    asChild
+                  >
+                    <a href={social.url}>
+                      <social.icon className="size-4" />
+                    </a>
+                  </Button>
+                ))}
+              </div>
+              <div className="hidden flex-col gap-x-1 font-mono text-sm text-foreground/80 print:flex print:text-[12px]">
+                {RESUME_DATA.contact.email ? (
                   <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                    <MailIcon className="size-4" />
+                    <span className="underline">
+                      {RESUME_DATA.contact.email}
+                    </span>
                   </a>
-                </Button>
-              ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <Button
-                  className="size-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
+                ) : null}
+                {RESUME_DATA.contact.tel ? (
                   <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                    <PhoneIcon className="size-4" />
+                    <span className="underline">{RESUME_DATA.contact.tel}</span>
                   </a>
-                </Button>
-              ) : null}
-              {RESUME_DATA.contact.social.map((social) => (
-                <Button
-                  key={social.title}
-                  className="size-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href={social.url}>
-                    <social.icon className="size-4" />
-                  </a>
-                </Button>
-              ))}
+                ) : null}
+              </div>
             </div>
-            <div className="hidden flex-col gap-x-1 font-mono text-sm text-foreground/80 print:flex print:text-[12px]">
-              {RESUME_DATA.contact.email ? (
-                <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                  <span className="underline">{RESUME_DATA.contact.email}</span>
-                </a>
-              ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                  <span className="underline">{RESUME_DATA.contact.tel}</span>
-                </a>
-              ) : null}
-            </div>
-          </div>
 
-          <Avatar className="size-28">
-            <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
-            <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
-          </Avatar>
+            <Avatar className="size-28">
+              <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
+              <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
+            </Avatar>
+          </div>
         </div>
-        <Section>
-          <h2 className="text-xl font-bold">About</h2>
-          <p className="text-pretty font-mono text-sm text-foreground/80 print:text-[12px]">
-            {RESUME_DATA.summary}
-          </p>
-        </Section>
-        <Section>
-          <h2 className="text-xl font-bold">Work Experience</h2>
-          {RESUME_DATA.work.map((work) => {
-            return (
-              <Card key={work.company}>
-                <CardHeader>
-                  <div className="flex flex-col justify-between gap-x-2 gap-y-1 text-base">
-                    <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
-                      <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
-                        <h3 className="font-semibold leading-none">
-                          {work.href ? (
-                            <a className="hover:underline" href={work.href}>
-                              {work.company}
-                            </a>
-                          ) : (
-                            <span>{work.company}</span>
-                          )}
-                        </h3>
-                        <div className="flex flex-wrap gap-1">
-                          {work.badges.map((badge) => (
-                            <Badge
-                              variant="secondary"
-                              className="text-xs print:px-1 print:py-0.5 print:text-[8px] print:leading-tight"
-                              key={badge}
-                            >
-                              {badge}
-                            </Badge>
-                          ))}
+        <div className="animate-fade-in" style={{ animationDelay: "75ms" }}>
+          <Section>
+            <h2 className="text-xl font-bold">About</h2>
+            <p className="text-pretty font-mono text-sm text-foreground/80 print:text-[12px]">
+              {RESUME_DATA.summary}
+            </p>
+          </Section>
+        </div>
+        <div className="animate-fade-in" style={{ animationDelay: "150ms" }}>
+          <Section>
+            <h2 className="text-xl font-bold">Work Experience</h2>
+            {RESUME_DATA.work.map((work) => {
+              return (
+                <Card key={work.company}>
+                  <CardHeader>
+                    <div className="flex flex-col justify-between gap-x-2 gap-y-1 text-base">
+                      <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
+                        <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
+                          <h3 className="font-semibold leading-none">
+                            {work.href ? (
+                              <a className="hover:underline" href={work.href}>
+                                {work.company}
+                              </a>
+                            ) : (
+                              <span>{work.company}</span>
+                            )}
+                          </h3>
+                          <div className="flex flex-wrap gap-1">
+                            {work.badges.map((badge) => (
+                              <Badge
+                                variant="secondary"
+                                className="text-xs print:px-1 print:py-0.5 print:text-[8px] print:leading-tight"
+                                key={badge}
+                              >
+                                {badge}
+                              </Badge>
+                            ))}
+                          </div>
+                        </div>
+                        <div className="text-sm tabular-nums text-gray-500">
+                          {work.start} - {work.end ?? "Present"}
                         </div>
                       </div>
+                    </div>
+
+                    <h4 className="font-mono text-sm font-semibold leading-none print:text-[12px]">
+                      {work.title}
+                    </h4>
+                  </CardHeader>
+                  <ul>
+                    {work.description.map((desc) => (
+                      <li
+                        className="mt-2 list-inside list-disc font-mono text-xs text-foreground/80 print:text-[10px]"
+                        key={desc}
+                      >
+                        {desc}
+                      </li>
+                    ))}
+                  </ul>
+                </Card>
+              );
+            })}
+          </Section>
+        </div>
+        <div className="animate-fade-in" style={{ animationDelay: "225ms" }}>
+          <Section>
+            <h2 className="text-xl font-bold">Education</h2>
+            {RESUME_DATA.education.map((education) => {
+              return (
+                <Card key={education.institution}>
+                  <CardHeader>
+                    <div className="flex items-center justify-between gap-x-2 text-base">
+                      <h3 className="font-semibold leading-none hover:underline">
+                        <a
+                          href={education.href}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          {education.institution}
+                        </a>
+                      </h3>
                       <div className="text-sm tabular-nums text-gray-500">
-                        {work.start} - {work.end ?? "Present"}
+                        {education.start} - {education.end}
                       </div>
                     </div>
-                  </div>
-
-                  <h4 className="font-mono text-sm font-semibold leading-none print:text-[12px]">
-                    {work.title}
-                  </h4>
-                </CardHeader>
-                <ul>
-                  {work.description.map((desc) => (
-                    <li
-                      className="mt-2 list-inside list-disc font-mono text-xs text-foreground/80 print:text-[10px]"
-                      key={desc}
-                    >
-                      {desc}
-                    </li>
-                  ))}
-                </ul>
-              </Card>
-            );
-          })}
-        </Section>
-        <Section>
-          <h2 className="text-xl font-bold">Education</h2>
-          {RESUME_DATA.education.map((education) => {
-            return (
-              <Card key={education.institution}>
-                <CardHeader>
-                  <div className="flex items-center justify-between gap-x-2 text-base">
-                    <h3 className="font-semibold leading-none hover:underline">
-                      <a href={education.href} target="_blank" rel="noreferrer">
-                        {education.institution}
+                  </CardHeader>
+                  <CardContent className="mt-2 text-foreground/80 print:text-[12px]">
+                    {education.degree}
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </Section>
+        </div>
+        <div className="animate-fade-in" style={{ animationDelay: "300ms" }}>
+          <Section>
+            <h2 className="text-xl font-bold">Honors and Awards</h2>
+            {RESUME_DATA.honors.map((honor) => {
+              return (
+                <Card key={honor.title}>
+                  <CardHeader>
+                    <h3 className="text-base font-semibold leading-none hover:underline">
+                      <a href={honor.href} target="_blank" rel="noreferrer">
+                        {honor.title}
                       </a>
                     </h3>
-                    <div className="text-sm tabular-nums text-gray-500">
-                      {education.start} - {education.end}
-                    </div>
-                  </div>
-                </CardHeader>
-                <CardContent className="mt-2 text-foreground/80 print:text-[12px]">
-                  {education.degree}
-                </CardContent>
-              </Card>
-            );
-          })}
-        </Section>
-        <Section>
-          <h2 className="text-xl font-bold">Honors and Awards</h2>
-          {RESUME_DATA.honors.map((honor) => {
-            return (
-              <Card key={honor.title}>
-                <CardHeader>
-                  <h3 className="text-base font-semibold leading-none hover:underline">
-                    <a href={honor.href} target="_blank" rel="noreferrer">
-                      {honor.title}
-                    </a>
-                  </h3>
-                </CardHeader>
-                <CardContent className="mt-2 text-foreground/80 print:text-[12px]">
-                  {honor.description}
-                </CardContent>
-              </Card>
-            );
-          })}
-        </Section>
-        <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
-          <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.skills.map((skill) => {
-              return (
-                <Badge className="print:text-[10px]" key={skill}>
-                  {skill}
-                </Badge>
+                  </CardHeader>
+                  <CardContent className="mt-2 text-foreground/80 print:text-[12px]">
+                    {honor.description}
+                  </CardContent>
+                </Card>
               );
             })}
-          </div>
-        </Section>
+          </Section>
+        </div>
+        <div className="animate-fade-in" style={{ animationDelay: "375ms" }}>
+          <Section>
+            <h2 className="text-xl font-bold">Skills</h2>
+            <div className="flex flex-wrap gap-1">
+              {RESUME_DATA.skills.map((skill) => {
+                return (
+                  <Badge className="print:text-[10px]" key={skill}>
+                    {skill}
+                  </Badge>
+                );
+              })}
+            </div>
+          </Section>
+        </div>
 
-        <Section className="print-force-new-page scroll-mb-16">
-          <h2 className="text-xl font-bold">Side Projects</h2>
-          <div className="-mx-3 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 print:gap-2">
-            {RESUME_DATA.projects.map((project) => {
-              return (
-                <ProjectCard
-                  key={project.title}
-                  title={project.title}
-                  description={project.description}
-                  tags={project.techStack}
-                  href={project.href}
-                />
-              );
-            })}
-          </div>
-        </Section>
+        <div className="animate-fade-in" style={{ animationDelay: "450ms" }}>
+          <Section className="print-force-new-page scroll-mb-16">
+            <h2 className="text-xl font-bold">Side Projects</h2>
+            <div className="-mx-3 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 print:gap-2">
+              {RESUME_DATA.projects.map((project) => {
+                return (
+                  <ProjectCard
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    tags={project.techStack}
+                    href={project.href}
+                  />
+                );
+              })}
+            </div>
+          </Section>
+        </div>
       </section>
 
       <CommandMenu
